2dd48397dadf9a9cad15998789874354d0e78fa2
bram <bramkaashoek@gmail.com>
2017-06-26 21:00:44 +0200

    initial commit

---
+++ src/models/batches.model.js
@@ -0,0 +1,35 @@
+// batches-model.js - A mongoose model
+//
+// See http://mongoosejs.com/docs/models.html
+// for more of what you can do here.
+module.exports = function (app) {
+  const mongooseClient = app.get('mongooseClient');
+  const { Schema } = mongooseClient;
+
+  const evaluationsSchema = new Schema({
+    color: { type: Number, required: true },
+    date: { type: Date, default: Date.now },
+    remarks: { type: String },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now }
+  });
+
+  const studentsSchema = new Schema({
+    name: { type: String, required: true },
+    profilePicture: { type: String, required: true },
+    evaluations: [evaluationsSchema],
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now }
+  });
+
+  const batches = new Schema({
+    batchNumber: { type: Number, required: true },
+    startDate: { type: Date, required: true },
+    endDate: { type: Date, required: true },
+    students: [studentsSchema],
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now }
+  });
+
+  return mongooseClient.model('batches', batches);
+};
