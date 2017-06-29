289e0711f8a3f2386f5e883ff7154f1a4ef31c22
bram <bramkaashoek@gmail.com>
2017-06-29 17:46:11 +0200

    create batch

--- src/hooks/create-batch.js
+++ src/hooks/create-batch.js
@@ -5,14 +5,16 @@ module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
   return function (hook) {
     return hook.app.service('batches').find().then((batches) =>{
 
-      const highestBatchNumber = batches.data.reduce((prev, next) => {
-        console.log(prev)
-        console.log(next.batchNumber)
-        //console.log(next)
-        return Math.max(prev, next.batchNumber)
-      },[0])
+      if (hook.data.batchNumber === undefined) {
+        const highestBatchNumber = batches.data.reduce((prev, next) => {
+          console.log(prev)
+          console.log(next.batchNumber)
+          //console.log(next)
+          return Math.max(prev, next.batchNumber)
+        },[0])
 
-      hook.data.batchNumber  = highestBatchNumber + 1
+        hook.data.batchNumber  = highestBatchNumber + 1
+      }
     })
   }
 };
